ef2dbb7638ca26c6042ab186c7c0dc51ab732035
超超 <qianchao@jinlanzuan.com>
2021-06-07 21:09:00 +0800

    bug fix

---
+++ src/Traits/ApiResponse.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace CCTools\Traits;
+
+
+use Illuminate\Support\Facades\Response;
+
+trait ApiResponse
+{
+    public function success($msg,$data,$code = 200)
+    {
+        $result = [];
+        $msg ? $result['msg'] = $msg : $result['msg'] = 'ok';
+        $result['code'] = $code;
+        $result['data'] = $data;
+        return response()->json($result,200);
+    }
+
+
+    public function error($msg,$code = 500)
+    {
+        $result = [];
+        $msg ? $result['msg'] = $msg : $result['msg'] = 'fail';
+        $result['code'] = $code;
+        return response()->json($result,200);
+    }
+}
