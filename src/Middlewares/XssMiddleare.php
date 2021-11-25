ec295be912f70d10daa4957a3254aafa1541a780
超超 <qianchao@jinlanzuan.com>
2021-11-25 12:21:32 +0800

    增加XSS防御中间件

---
+++ src/Middlewares/XssMiddleare.php
@@ -0,0 +1,25 @@
+<?php
+namespace CCTools\Middlewares;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class XssMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $input = $request->all();
+        array_walk_recursive($input,function(&$input){
+            $input = strip_tags($input);
+        });
+        $request->merge($input);
+        return $next($request);
+    }
+}
