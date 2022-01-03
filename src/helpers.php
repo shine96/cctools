3a05e1f544e1b178e97a9c2ae417e9ce8bb9cef2
超超 <qianchao@jinlanzuan.com>
2022-01-03 21:08:51 +0800

    增加辅助函数

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,9 @@
 <?php
+
+if (! function_exists('milliseconds')) {
+    function milliseconds(): float
+    {
+        list($msec,$sec) = explode(' ',microtime());
+        return (float)sprintf('%.0f', (floatval($msec) + floatval($sec)) * 1000);
+    }
+}
